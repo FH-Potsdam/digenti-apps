79e48e85bc7dfcc574895278a42fb7849b9b1058
Jordi Tost <tost.jordi@gmail.com>
2016-08-29 16:04:26 +0200

    app

---
+++ app/js/isolinesLayer.js
@@ -0,0 +1,353 @@
+function isolinesLayer(svg) {
+
+
+    //////////////////////
+    // Base
+    //////////////////////
+    var parent = this;
+
+
+    //////////////////////
+    // Variables of the Layer
+    //////////////////////
+    //this.svglayer = "";
+    this.circleRadius = 5;
+    this.isolineColor = '#3dc8e7';
+    this.isolineOpacity = 0.35;
+    this.isolinesQueried = 0;
+    this.bcr = [];
+
+    this.isolinesGeoJSON = {
+        "type":"FeatureCollection",
+        "crs":{
+            "type":"name",
+            "properties":{
+                "name":"urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features":[]
+    };
+
+
+    //////////////////////
+    // Functions
+    //////////////////////
+
+    this.setActive = function (state) {
+        this.active = state;
+        this.svglayer
+            .transition()
+            .duration(500)
+                .style("opacity", function() {
+                    if (state) { return 1; }
+                    else { return 0; }
+                });
+    }
+
+
+
+    // This callback is called when clicking on a location
+    function click(d, objectID) {
+        // var coordinates = d.geometry.coordinates;
+        getIsoline(d, objectID);
+
+    }
+
+    function getIsoline(d, objectID) {
+
+        var coordinates = d.geometry.coordinates;
+
+        var coordsStr = coordinates[1]+','+coordinates[0];
+        //var range = parseInt($("#range__slider").val());
+        var range = 30;
+
+        var uri = 'http://localhost:61002/api/isoline/' + coordsStr + '/' + range;
+
+        // Define a callback function to process the isoline response.
+        var onIsolineResult = function(result) {
+
+            this.isolinesQueried++;
+
+            var polygon = result;
+
+            // polygon.properties.objectsID = objectID;
+
+            var settlementPoint = {
+                "type": "Feature",
+                "properties": {
+                    "marker-color": "#f00"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": coordinates
+                }
+            };
+
+            var polygonBuffered = turf.buffer(polygon, 2000, "meters");
+
+            var isInside = turf.inside(settlementPoint, polygonBuffered.features[0]);
+
+            if (isInside) {
+
+                // Add OSM ID to polygon
+                polygon.properties.osm_id = d.properties.osm_id;
+                polygon.properties.name = d.properties.name;
+
+                // Save polygon in GeoJSON FeaturesCollection
+                parent.isolinesGeoJSON.features.push(polygon);
+
+                // Isoline group
+                var g = parent.svglayer.select('g[data-id="'+objectID+'"]').select(".isoline-group-vis");
+
+                // d3 isoline
+                var isoline = g.append("path")
+                        		.data([polygon])
+                                .attr("class", "isoline")
+                                .attr("data-id", objectID);
+
+            }
+
+            // Update isolines when all loaded
+            if (this.isolinesQueried === places_aoi.features.length) {
+                this.update(500);
+                console.log("FERTIG");
+                //activateButtons();
+            }
+        };
+
+        $.ajax({
+            dataType: "json",
+            url: uri,
+            //   url: 'http://localhost:61002/api/isoline/',
+            //   data: {
+            //       coords: coords,
+            //       range: range
+            //   },
+            success: onIsolineResult,
+            error: function(error) {
+                alert(error);
+            }
+        });
+    }
+
+
+
+    /**
+     * initializes the layer
+     * @param {Number} svg
+     * @param {geojson} geojson
+     */
+    this.init = function (svg, geojson) {
+
+        this.svglayer = svg.append("g").attr("class", "isolines");
+        this.setActive(false);
+
+        this.svglayer
+                .selectAll("circle")
+                .data(geojson.features)
+                .enter()
+                .append("g")
+                    .attr("data-id", function(d) { return d.properties.osm_id; })
+                    .attr("class", "isoline-group")
+                    .each(function(d) {
+                        var current_el = d3.select(this);
+                        current_el.append("rect")
+                            .attr("class", "layoutdebug");
+                    })
+                    .append("g")
+                        .attr("class", "isoline-group-vis")
+                        .each(function(d) {
+                            var current_el = d3.select(this);
+
+                            current_el.append("circle")
+                                .attr({
+                                    "r": config.circleRadius
+                                })
+                                .attr("class", "village")
+                                .attr("data-id", function(d) { return d.properties.osm_id; })
+                                .each(function(d) {
+                                    getIsoline(d, d.properties.osm_id);
+                                });
+                        });
+
+
+
+    }
+
+
+
+    /**
+     * updates the view of the layer
+     * @param {Number} transition_time
+     */
+    this.update = function (transition_time) {
+
+        // Path transform
+        var transform = d3.geo.transform({point: projectPoint});
+    	var path = d3.geo.path().projection(transform);
+
+        if (isDefined(this.svglayer)) {
+
+            if (config.view === "smallmultiples") {
+
+                // Transform isolines
+                var ix = 0;
+                var iy = 0;
+
+                var max_path_w = 0;
+                var max_path_h = 0;
+
+                this.svglayer.selectAll(".isoline-group-vis").each(function(d, i) {
+
+                    var current_el = d3.select(this);
+
+                    // Check max width / height
+                    var current_path_w = current_el.node().getBBox().width;
+                    var current_path_h = current_el.node().getBBox().height;
+
+                    if (current_path_h>max_path_h) { max_path_h = current_path_h; }
+                    if (current_path_w>max_path_w) { max_path_w = current_path_w; }
+
+                });
+
+                var widthperelement = (config.layout.w - config.layout.offsetLeft - config.layout.offsetRight - (config.layout.cols-1)*config.layout.gapX) / config.layout.cols;
+                var heightperelement = (config.layout.h - config.layout.offsetTop - config.layout.offsetBottom - (config.layout.rows-1)*config.layout.gapY) / config.layout.rows;
+
+                var faktor_height = heightperelement/max_path_h;
+                var faktor_width = widthperelement/max_path_w;
+
+                var scaleFactor = faktor_height;
+                if (faktor_width<scaleFactor) { scaleFactor=faktor_width; }
+
+                this.svglayer.selectAll(".isoline-group").each(function(d, index) {
+
+                    var current_el = d3.select(this);
+
+                    if (layoutdebug === true) {
+                        current_el.selectAll(".layoutdebug")
+                            .attr("fill", "rgba(255, 0, 255, 0.3)")
+                            .attr("width", widthperelement)
+                            .attr("height", heightperelement);
+                    } else {
+                        current_el.selectAll(".layoutdebug")
+                            .attr("fill", "none")
+                            .attr("width", widthperelement)
+                            .attr("height", heightperelement);
+                    }
+
+                    current_el
+                        .transition()
+                        .duration(transition_time)
+                            .style("opacity", 1)
+                            .attr("transform", function() {
+                                var x = config.layout.offsetLeft + (ix-1)*config.layout.gapX + ix*widthperelement;
+                                var y = config.layout.offsetTop + (iy-1)*config.layout.gapY + iy*heightperelement;
+                                ix++;
+                                if (ix === config.layout.cols) { ix = 0; }
+                                iy++;
+                                if (iy === config.layout.rows) { iy = 0; }
+                                return "translate("+x+","+y+")";
+                            });
+
+                });
+
+
+                // Update isolines
+                this.svglayer.selectAll(".isoline-group-vis").each(function(d, index) {
+
+                    var current_el = d3.select(this);
+
+                    current_el
+                        .transition()
+                        .duration(transition_time)
+                            .style("opacity", 1)
+                            .attr("transform", function() {
+                                var bbox = d3.select(this).node().getBBox();
+                                var x = -bbox.x + ((widthperelement/scaleFactor)/2) - (bbox.width/2);
+                                var y = -bbox.y + ((heightperelement/scaleFactor)/2) - (bbox.height/2);
+                                return "scale("+scaleFactor+") translate("+x+","+y+")";
+                            });
+
+                    current_el.selectAll("circle")
+                        .transition()
+                        .delay(transition_time/6)
+                        .duration(transition_time)
+                        .attr({
+                            "r": config.circleRadius/scaleFactor
+                        })
+                        .each(function() {
+                            parent.bcr[d3.select(this).attr("data-id")] = d3.select(this).node().getBoundingClientRect();
+                        });
+                });
+
+                // Update isolines
+                this.svglayer.selectAll(".isoline").each(function(d, index) {
+                    var isoline = d3.select(this);
+                    isoline.attr("d", path);
+                });
+
+
+
+            } else {
+
+                this.svglayer.selectAll(".isoline-group").each(function(d, i) {
+
+                    var current_el = d3.select(this);
+
+                    current_el
+                        .transition()
+                        .duration(transition_time)
+                            .style("opacity", 1)
+                            .attr("stroke-width", 2)
+                            .attr("transform", function() { return ""; });
+
+                });
+
+
+                this.svglayer.selectAll(".isoline-group-vis").each(function(d, i) {
+
+                    var current_el = d3.select(this);
+
+                    current_el
+                        .transition()
+                        .duration(transition_time)
+                            .attr("transform", function() { return ""; });
+                });
+
+                // Update villages
+                this.svglayer.selectAll("circle").each(function() {
+
+                    var current_el = d3.select(this);
+
+                    //console.log(current_el.node().getBoundingClientRect());
+
+                    current_el
+                        .transition()
+                        .duration(transition_time)
+                            .attr({ "r": config.circleRadius })
+                            .attr({
+                                cx: function(d) { return project(d.geometry.coordinates).x; },
+                                cy: function(d) { return project(d.geometry.coordinates).y; },
+                            });
+
+                });
+
+                // Update isolines
+                this.svglayer.selectAll(".isoline").each(function(d, index) {
+                    var isoline = d3.select(this);
+                    isoline.attr("d", path);
+                });
+
+                setMapOpacity(1);
+
+
+            }
+
+        }
+
+
+
+    }
+
+
+}
---
+++ app/js/main.js
@@ -0,0 +1,457 @@
+/*global d3:true */
+/*global mapboxgl:true */
+/*global turf:true */
+/*global console:true */
+
+
+/* #############
+        HERE CONFIG
+   ############# */
+
+var platform = new H.service.Platform({
+  'app_id': 'EOg7UyuSFbPF0IG5ANjz',
+  'app_code': 'iRnqNl0dyzX_8FOlchD0ZQ'
+});
+
+// Get an instance of the normal routing service:
+var router = platform.getRoutingService();
+
+
+
+
+
+
+
+
+/* #############
+        VARS
+   ############# */
+
+var coord_valledupar = "10.471667,-73.25";
+var layoutdebug = false;
+var $body;
+var theme = 'light';
+var map;
+var svg;
+var settlementPointLayer;
+var lineFunction;
+var places_aoi, street_points_aoi;
+var routesArray = [];
+var routesJSON = {};
+routesJSON.routes = [];
+
+// CONFIG ARRAY
+var config  = {};
+config.view = "";
+config.mode = "";
+config.orderby = "size";
+
+config.layout = {};
+
+config.circleRadius = 5;
+
+config.layers = {};
+
+
+/////////////
+// onReady
+/////////////
+
+$(document).ready(function (){
+
+    // Config
+    $body = $('body');
+
+    theme = ($body.hasClass('dark')) ? 'dark' : 'light';
+
+    console.log("theme: " + theme);
+
+    // Init layer configuration and data load
+    init();
+});
+
+
+function init() {
+    $.getScript("js/routesLayer.js", function(data, textStatus, jqxhr) {
+
+        config.layers.gsm = new routesLayer();
+
+        $.getScript("js/missingInfrastructureLayer.js", function(data, textStatus, jqxhr) {
+
+            config.layers.missingInfrastructure = new missingInfrastructureLayer();
+
+            $.getScript("js/isolinesLayer.js", function(data, textStatus, jqxhr) {
+
+                config.layers.isolines = new isolinesLayer();
+
+                d3.json("../../data/places_aoi.json", function(err, data) {
+
+                    places_aoi = data;
+
+                    d3.json("../../data/street_points_aoi.json", function(err, data2) {
+
+                        street_points_aoi = data2;
+                        //console.log(street_points_aoi);
+
+                        $.get("data/routes_cached.json")
+                            .done(function() {
+                                d3.json("data/routes_cached.json", function(error, data3) {
+                                    //routesJSON = data3;
+
+                                    for (var i = 0; i<routesJSON.routes.length; i++) {
+                                        //routesArray[routesJSON.routes[i].id] = routesJSON.routes[i].route
+                                    }
+
+                                    mapDraw(data);
+
+                                });
+                            })
+                            .fail(function() {
+                                mapDraw(data);
+                            });
+
+                    });
+
+                });
+
+            });
+
+        });
+
+    });
+}
+
+
+function mapDraw(geojson) {
+
+    mapboxgl.accessToken = 'pk.eyJ1Ijoiam9yZGl0b3N0IiwiYSI6ImQtcVkyclEifQ.vwKrOGZoZSj3N-9MB6FF_A';
+
+    var baseMap = (theme == 'dark') ? 'mapbox://styles/jorditost/cir1xojwe0020chknbi0y2d5t' : 'mapbox://styles/jorditost/ciqc61l3p0023dunqn9e5t4zi'
+
+    map = new mapboxgl.Map({
+        container: 'map',
+        style: baseMap,
+        zoom: 11,
+        center: [-73.12, 10.410]
+    });
+
+    map.addControl(new mapboxgl.Navigation());
+
+    // Map functions
+    map.on("viewreset", update);
+    map.on("moveend", update);
+    map.on("move", update);
+
+    map.on("load", test456);
+
+    function test456() {
+        $("#loader").removeClass("show");
+        setTimeout(function() {
+            $("#loader").removeClass("displayed");
+        }, 1000);
+    }
+
+    // d3 canvas
+    svg = d3.select(map.getCanvasContainer()).append("svg").attr("class", "map-features");
+
+
+    //This is the accessor function we talked about above
+    lineFunction = d3.svg.line()
+                        .x(function(d) { return project(d).x; })
+                        .y(function(d) { return project(d).y; })
+                        .interpolate("linear");
+
+    settlementPointLayer = svg.append("g").attr("class", "settlementPointLayer").style("opacity", 1);
+
+    settlementPointLayer.selectAll("circle")
+        .data(places_aoi.features)
+        .enter()
+        .append("circle")
+            .attr("class", "village")
+            .attr("r", config.circleRadius)
+            .attr("data-id", function(d) { return d.properties.osm_id; });
+
+    // Initialize the Layers
+    config.layers.gsm.init(svg, geojson);
+    config.layers.missingInfrastructure.init(svg, geojson);
+    config.layers.isolines.init(svg, geojson);
+
+    triggerMapView();
+    setMode("missinginfrastructure");
+
+    // Inital Update to render Map
+    update(0);
+
+
+
+    var basemap_select = document.getElementById('basemap_select');
+    var basemap_select_options = basemap_select.options;
+
+    basemap_select.onchange = function() {
+        var selectedValue = basemap_select_options[basemap_select.selectedIndex].value;
+        switchLayer(selectedValue);
+    };
+
+    function switchLayer(layer) {
+        if (layer === 'DIGENTI') {
+            map.setStyle('mapbox://styles/jorditost/cipseaugm001ycunimvr00zea');
+        } else if (layer === 'DIGENTI-Light') {
+            map.setStyle('mapbox://styles/jorditost/ciqc61l3p0023dunqn9e5t4zi');
+        } else if (layer === 'DIGENTI-Dark') {
+            map.setStyle('mapbox://styles/jorditost/cir1xojwe0020chknbi0y2d5t');
+        } else if (layer === 'fos-outdoor') {
+            map.setStyle('mapbox://styles/jorditost/cip44ooh90013cjnkmwmwd2ft');
+        } else {
+            map.setStyle('mapbox://styles/mapbox/' + layer);
+        }
+    }
+
+
+
+
+
+}
+
+
+
+function update(transition_time) {
+
+    // Set transition time if it is undefined
+    if (isNaN(transition_time)) { transition_time = 0; }
+
+    config.layout = calculateLayoutVars();
+
+    config.layers.gsm.update(transition_time);
+    config.layers.missingInfrastructure.update(transition_time);
+    config.layers.isolines.update(transition_time);
+
+}
+
+
+
+
+
+
+
+
+///////////////////
+// TRIGGER VIEWS
+///////////////////
+
+function reorderSmallMultiples(ob) {
+    config.orderby = ob;
+    d3.selectAll(".orderby").classed("active", false);
+    d3.selectAll("."+config.orderby).classed("active", true);
+    update(500);
+}
+
+function setMode(mode) {
+
+    config.mode = mode;
+    console.log("Set Mode: "+config.mode);
+
+    var timeout = 0;
+    if (config.view === "smallmultiples") { timeout = 500; }
+
+    d3.selectAll(".mode").classed("active", false);
+
+    if (config.mode === "missinginfrastructure") {
+        d3.selectAll(".mode.missinginfrastructure").classed("active", true);
+        config.layers.gsm.setActive(false);
+        config.layers.isolines.setActive(false);
+        if (config.view === "smallmultiples") { updateSettlementPointLayer(); }
+        setTimeout(function() {
+            config.layers.missingInfrastructure.setActive(true);
+            update(500);
+        }, timeout);
+    } else if (config.mode === "routesfromvalledupar") {
+        d3.selectAll(".mode.routesfromvalledupar").classed("active", true);
+        config.layers.missingInfrastructure.setActive(false);
+        config.layers.isolines.setActive(false);
+        if (config.view === "smallmultiples") { updateSettlementPointLayer(); }
+        setTimeout(function() {
+            config.layers.gsm.setActive(true);
+            update(500);
+        }, timeout);
+    } else if (config.mode === "isolines") {
+        d3.selectAll(".mode.isolines").classed("active", true);
+        config.layers.missingInfrastructure.setActive(false);
+        config.layers.gsm.setActive(false);
+        if (config.view === "smallmultiples") { updateSettlementPointLayer(); }
+        setTimeout(function() {
+            config.layers.isolines.setActive(true);
+            update(500);
+        }, timeout);
+    }
+
+
+}
+
+function triggerMapView() {
+    d3.selectAll(".view").classed("active", false);
+    d3.selectAll(".mapview").classed("active", true);
+    d3.selectAll("#orderby").classed("disabled", true);
+
+    enableMapInteraction();
+
+    config.view = "";
+    update(500);
+}
+
+function triggerMapDistancesView() {
+    d3.selectAll(".view").classed("active", false);
+    d3.selectAll(".mapdistancesview").classed("active", true);
+    d3.selectAll("#orderby").classed("disabled", true);
+
+    enableMapInteraction();
+
+    config.view = "map-distances";
+    update(500);
+}
+
+function triggerSmallMultiplesView() {
+    d3.selectAll(".view").classed("active", false);
+    d3.selectAll(".smallmultiplesview").classed("active", true);
+    d3.selectAll("#orderby").classed("disabled", false);
+
+    disableMapInteraction();
+
+    config.view = "smallmultiples";
+    update(500);
+}
+
+function setMapOpacity(value) {
+
+    d3.selectAll(".mapboxgl-canvas")
+        .transition()
+        .duration(500)
+            .style("opacity", value);
+
+    d3.selectAll(".mapboxgl-control-container")
+        .transition()
+        .duration(500)
+            .style("opacity", value);
+}
+
+
+function activateButtons() {
+    d3.selectAll(".disabled")
+        .attr("disabled", null);
+}
+
+
+
+
+
+
+
+
+function updateSettlementPointLayer() {
+
+    if (config.mode === "isolines") {
+        test123(config.layers.isolines.bcr)
+    } else if (config.mode === "routesfromvalledupar") {
+        test123(config.layers.gsm.bcr);
+    } else if (config.mode === "missinginfrastructure") {
+        test123(config.layers.missingInfrastructure.bcr);
+    }
+
+}
+
+function test123(bcr) {
+
+    if (isDefined(bcr)) {
+
+        settlementPointLayer.selectAll("circle")
+            .each(function() {
+                var current_el = d3.select(this);
+                var current_id = current_el.attr("data-id");
+                if (isDefined(bcr[current_id])) {
+                    current_el
+                        .attr("opacity", "1")
+                        .transition()
+                        .each("end", function() {
+                            current_el
+                                .transition()
+                                .duration(500)
+                                    .attr("opacity", "0");
+                        })
+                        .duration(500)
+                            .attr("cx", bcr[current_id].left+config.circleRadius)
+                            .attr("cy", bcr[current_id].top+config.circleRadius);
+                }
+            });
+
+
+    }
+
+}
+
+
+
+
+
+
+//////////////////////
+// Map Interactions
+//////////////////////
+
+function enableMapInteraction() {
+    map.scrollZoom.enable();
+    map.dragPan.enable();
+    d3.select("#map").classed("disabled", false);
+}
+
+function disableMapInteraction() {
+    map.scrollZoom.disable();
+    map.dragPan.disable();
+    d3.select("#map").classed("disabled", true);
+}
+
+
+
+
+
+
+
+function selectSettlement(id) {
+    console.log(id);
+    $( "p.objectID" ).html( id );
+}
+
+
+function calculateLayoutVars() {
+
+    var layoutVars = {};
+    layoutVars.w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    layoutVars.h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    layoutVars.rows = 7;
+    layoutVars.cols = 6;
+    layoutVars.gap_hor = (layoutVars.w*0.8)/(layoutVars.cols+1);
+    layoutVars.gap_ver = (layoutVars.h)/(layoutVars.rows+1);
+    layoutVars.offsetLeft = layoutVars.w*0.25;
+    layoutVars.offsetRight = 30;
+    layoutVars.offsetTop = layoutVars.h*0.1;
+    layoutVars.offsetBottom = 30;
+    layoutVars.gapX = 15;
+    layoutVars.gapY = 10;
+
+    return layoutVars;
+
+}
+
+
+
+function project(d) {
+    return map.project(new mapboxgl.LngLat(+d[0], +d[1]));
+}
+
+function projectPoint(lon, lat) {
+    var point = map.project(new mapboxgl.LngLat(lon, lat));
+    this.stream.point(point.x, point.y);
+}
+
+
+function isDefined(v) {
+    return (typeof v !== 'undefined' && v !== null);
+}

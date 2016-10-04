07631110387b5dd31b4ada102ccb06136dbb8c99
Jordi Tost <tost.jordi@gmail.com>
2016-10-04 17:57:44 +0200

    Microvisualizations in infobox started

--- app/js/routesLayer.js
+++ app/js/routesLayer.js
@@ -99,9 +99,7 @@ function routesLayer() {
 
                     // push route geometry to routes_geo-Array
                     parent.routes_geo[uid] = route.geometry.coordinates;
-
                 }
-
             }
 
             // succeeded!

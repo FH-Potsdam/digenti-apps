ecef6aaf3fe93ec56c3755d7fd97efb5fa704349
Jordi Tost <tost.jordi@gmail.com>
2018-05-03 15:48:12 +0200

    Added more parameters to config.sample.js

--- app-access/config.sample.js
+++ app-access/config.sample.js
@@ -1,10 +1,16 @@
 var config = {};
 
-// Mapbox
-config.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
-
 // Tabletop
 config.tabletop = false;
+
+// Theme
+config.theme = "dark"; // Possible values: 'light', 'dark'. Default: 'light'
+
+// Dev
+config.noUI = false;
+
+// Mapbox
+config.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
 
 // DIGENTI API
 config.apiBase = "http://localhost:61002/api";
@@ -18,18 +24,25 @@ config.map = {};
 config.map.zoom = 11.2;
 config.map.center = [-73.09, 10.422];
 
-// Theme
-config.theme = "dark"; // Possible values: 'light', 'dark'. Default: 'light'
-config.noUI = false;
-
 // Routes origin
 config.coordHomeBase = "10.471667,-73.25";
 
 // Show threat data
 config.threat = {};
 config.threat.show = false;
+config.threat.showMissing = false;
 config.threat.buffer = 350;
 config.threat.intersect = true;
+config.threat.opacity = 0.75;
+
+// Show landing sites
+config.landing = {};
+config.landing.show = true;
+config.landing.ndviThres = 2;
+config.landing.gradientThres = 3;
+config.landing.buffer = 1800;
+config.landing.intersect = true;
+config.landing.opacity = 0.45;
 
 // Settlements mode
 config.multipleSettlements = false;
